c00bc528c76dac371df9cac25e6bd6070f701ac0
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2022-10-06 23:27:45 +0530

    Add files via upload

---
+++ Lab Assessment(6 Oct)/Encapsulation/ClashofClans.java
@@ -0,0 +1,38 @@
+package encap;
+
+public class ClashofClans {
+	private String loot;
+	private int elixir;
+	private int coins;
+	
+	
+	public void setLoot(String newloot) {
+		loot = newloot;
+	}
+
+	
+	public String getLoot() {
+		return loot;
+	}
+	
+	
+	public void setElixir(int newElixir) {
+		elixir = newElixir;
+	}
+
+
+	public int getElixir() {
+		return elixir;
+	}
+	
+	
+	public void setCoins(int newCoins) {
+		coins = newCoins;
+	}
+
+
+	public int getCoins() {
+		return coins;
+	}
+	
+}
