bf946f67411648700a0d9d4eaef5bf324a7c8356
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2022-10-07 23:23:29 +0530

    Delete Character.java

--- Lab Assessment(6 Oct)/Personjava Question1/Character.java
+++
@@ -1,8 +0,0 @@
-public class Character extends Person{
-    void personId(){
-        System.out.println("Your current id is 335");
-    }
-    void personName(){
-        System.out.println("Your current name is Kaustubh");
-    }
-}
