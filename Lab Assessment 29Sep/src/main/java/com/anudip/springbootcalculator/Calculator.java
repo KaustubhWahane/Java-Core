e4418de1f137cf9360f8622d784ec5c30f5abf5f
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2022-12-31 00:39:12 +0530

    Add files via upload

---
+++ Lab Assessment 29Sep/src/main/java/com/anudip/springbootcalculator/Calculator.java
@@ -0,0 +1,59 @@
+package com.anudip.springbootcalculator;
+
+import java.util.Scanner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class Calculator {
+	
+	@RequestMapping("/calculate")
+	public double calculate() {
+    char operator;
+    double number1, number2, result = 0;
+
+    @SuppressWarnings("resource")
+	Scanner input = new Scanner(System.in);
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
+
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {
+      case '+':
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
+
+      case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    
+
+
+  }
+    return result;
+}
+}
